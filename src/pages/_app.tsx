98fa5679fc04942ca800ccd27e53ac9dc84c2534
MJB2012 <matheusjbarbosa23@gmail.com>
2022-12-13 22:08:10 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,27 @@ import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
 import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   
   return (
-    <div>
-      <header>
-    
-       <img src={ logoImg }alt="" />
-      </header>
+    <Container>
+      <Header>
+        <Image 
+        src={logoImg}
+        alt="logo"
+        />
+      </Header>
+       
+      
       
       
       <Component {...pageProps} />
-    </div>
+    </Container>
   
   )
 }
