2e0890076f7ce2b39ccbf304c98964e5f835d940
MJB2012 <matheusjbarbosa23@gmail.com>
2022-12-14 20:39:10 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/image";
+
+import Shirt1 from 'src/assets/tshirt.png'
+import Shirt2 from 'src/assets/tshirt1.png'
+
+
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <HomeContainer>
+     
+      <Product>
+        <Image src={Shirt1} alt="shirt" width={520} height={480}/>
+        
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$30,00</span>
+       
+        </footer>
+      </Product>
+     
+      <Product>
+        <Image src={Shirt2} alt="shirt" width={520} height={480}/>
+        
+        <footer>
+          <strong>Shirt Y</strong>
+          <span>$30,00</span>
+        
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,14 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+    display: 'flex',
+    gap: '3rem',
+    width: '100%',
+    maxWidth: 'calc(100vw - ((100vw -1180px) / 2)',
+    marginLeft: 'auto',
+    minHeight: 656,
+});
+
+export const Product = styled('a', {
+    backgroundColor: 'linear-gradient( 180deg, #1ea483 0%, #7465d4 100% )'
+});
