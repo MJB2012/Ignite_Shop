1504dfbeafb9e060204d19f1f4ac47f14e5b8ad5
MJB2012 <matheusjbarbosa23@gmail.com>
2023-01-13 18:27:56 -0300

    checkout session

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,8 +1,17 @@
+import { resolveSoa } from 'dns';
 import { NextApiRequest, NextApiResponse } from 'next'
 import { stripe } from '../../lib/stripe';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const priceId = 'price_1MFjlBJutzOTxAC2fDZmj67f';
+    const { priceId } = req.body
+    
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' })
+    }
+
+    if(!priceId) {
+        return res.status(400).json({ error: 'Price not found' })
+    }
 
     const successUrl = `${process.env.NEXT_URL}/success`;
     const cancelUrl = `${process.env.NEXT_URL}/`;
